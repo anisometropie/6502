8b32678378f7b150020e85443ea7664e3b4975a8
Valentin Chanas <anisometropie@gmail.com>
2024-11-07 22:17:58 +0100

    update led

--- led.s
+++ led.s
@@ -1,28 +1,20 @@
+  ; Here we talk to the 65c22. set Port B pinsas output
+  ; then we write %10000000 to port B
   ; where we are in memory
   .org $8000
 
-  ; we first set all lines of Port B as output
-  ; by giving %11111111 to Data Direction Register B
 reset:
+  ; $6000 to 600F are connected to register select pins RS0, RS1, RS2, RS3
+  ; set all pins of Port B as output
+  ; by giving %11111111 to Data Direction Register B
   lda #%11111111
   sta $6002
 
-loop:
   lda #%10000000
   sta $6000
-  lda #%01000000
-  sta $6000
-  lda #%00100000
-  sta $6000
-  lda #%00010000
-  sta $6000
-  lda #%00001000
-  sta $6000
-  lda #%00000100
-  sta $6000
-  lda #%00000010
-  sta $6000
-  lda #%00000001
+
+loop:
+  ror
   sta $6000
 
   ; instead of writing jmp $8005, assembler will calculate the address of the label for us
